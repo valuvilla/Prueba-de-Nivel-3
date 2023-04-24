19d5588afe26c65b3cd2f75f65885fcaa9a3d351
valuvilla <valuvp@hotmail.com>
2023-04-24 13:33:35 +0200

    c4

--- implementacón.py
+++ implementacón.py
@@ -1,39 +1,75 @@
-class NodoArbol(object):
-    """clase para crear un nodo de un arbol"""
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
 
-    def __init__(self, info):
-        """crea un nodo con la informacion cargada"""
-        self.info = info
-        self.izq = None
-        self.der = None
+class BinaryTree:
+    def __init__(self):
+        self.root = None
 
-def insertar_marvel(raiz, pregunta, si, no):
-    """inserta un nodo en el arbol"""
-    if raiz is None:
-        return NodoArbol(pregunta)
-    else:
-        if  respuesta == "si":
-            raiz.izq = insertar_marvel(raiz.izq, si[0], si[1], si[2])
+    def insert(self, question, superhero, curr=None):
+        if self.root is None:
+            self.root = Node(question)
+            self.root.left = Node(superhero)
+            return
+
+        if curr is None:
+            curr = self.root
+
+        answer = input(curr.data + " (s/n): ").lower()
+        if answer == 's':
+            if curr.left is None:
+                curr.left = Node(superhero)
+                return
+            self.insert(question, superhero, curr.left)
+        elif answer == 'n':
+            if curr.right is None:
+                curr.right = Node(superhero)
+                return
+            self.insert(question, superhero, curr.right)
         else:
-            raiz.der = insertar_marvel(raiz.der, no[0], no[1], no[2])
-        return raiz
+            print("Por favor, responde con 's' o 'n'.")
+            self.insert(question, superhero, curr)
 
-def buscar_marvel(raiz, pregunta):
-    """busca un nodo en el arbol"""
-    if raiz is None or raiz.info == pregunta:
-        return raiz
-    elif pregunta[0] == "si":
-        return buscar_marvel(raiz.izq, pregunta[1])
-    else:
-        return buscar_marvel(raiz.der, pregunta[2])
+    def search(self, question, curr=None):
+        if self.root is None:
+            return None
 
-arbol = NodoArbol("Preg1")
-arbol.izq = NodoArbol("Preg2")
-arbol.izq.izq = NodoArbol("Hulk")
-arbol.izq.der = NodoArbol("Thor")
-arbol.der = NodoArbol("Preg3")
-arbol.der.izq = NodoArbol("IronMan")
-arbol.der.der = NodoArbol("Capitan")
+        if curr is None:
+            curr = self.root
 
-arbol = insertar_marvel(arbol, "¿El superhéroe tiene telarañas?", 
-                        ["Spiderman", None, None], ["Capitan", None, None])
+        answer = input(curr.data + " (s/n): ").lower()
+        if answer == 's':
+            if curr.left is None:
+                return None
+            elif curr.left.data in superheroes:
+                return curr.left.data
+            else:
+                return self.search(question, curr.left)
+        elif answer == 'n':
+            if curr.right is None:
+                return None
+            elif curr.right.data in superheroes:
+                return curr.right.data
+            else:
+                return self.search(question, curr.right)
+        else:
+            print("Por favor, responde con 's' o 'n'.")
+            return self.search(question, curr)
+
+superheroes = ['Thor', 'Hulk', 'Khan', 'Ant-Man', 'Capitán América', 'Capitana Marvel', 'The Winter Soldier', 'Nick Fury']
+tree = BinaryTree()
+
+# Insertar nodos
+tree.insert("¿Necesitas destruir un ejército completo?", "Thor")
+tree.insert("¿Necesitas destruir una gran cantidad de cosas?", "Hulk")
+tree.insert("¿Necesitas realizar una misión intergaláctica en equipo?", "Khan")
+tree.insert("¿Necesitas recuperar algo sin ser detectado?", "Ant-Man")
+tree.insert("¿Necesitas infiltrarte con personas del lugar?", "The Winter Soldier")
+tree.insert("¿Necesitas tomar una decisión rápida y moverte rápidamente?", "Nick Fury")
+tree.insert("¿Necesitas una persona para varias misiones?", "Khan")
+tree.insert("¿Necesitas viajar por las distintas galaxias?", "Capitana Marvel")
+tree.insert("¿Necesitas una persona para comandar misiones de defensa y de recuperación?", "Capitán América")
+
+

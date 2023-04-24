1b492aec78c5565d562ba0487a14ab31c9296d0c
valuvilla <valuvp@hotmail.com>
2023-04-24 13:33:21 +0200

    c4

--- implementacón.py
+++ implementacón.py
@@ -1,51 +1,39 @@
-class Node:
-    def __init__(self, question=None, left=None, right=None):
-        self.question = question
-        self.left = left
-        self.right = right
+class NodoArbol(object):
+    """clase para crear un nodo de un arbol"""
 
-def add_node(root, question, left=None, right=None):
-    new_node = Node(question, left, right)
-    if root is None:
-        root = new_node
+    def __init__(self, info):
+        """crea un nodo con la informacion cargada"""
+        self.info = info
+        self.izq = None
+        self.der = None
+
+def insertar_marvel(raiz, pregunta, si, no):
+    """inserta un nodo en el arbol"""
+    if raiz is None:
+        return NodoArbol(pregunta)
     else:
-        current_node = root
-        while current_node is not None:
-            last_node = current_node
-            if question.lower() < current_node.question.lower():
-                current_node = current_node.left
-            else:
-                current_node = current_node.right
-        if question.lower() < last_node.question.lower():
-            last_node.left = new_node
+        if  respuesta == "si":
+            raiz.izq = insertar_marvel(raiz.izq, si[0], si[1], si[2])
         else:
-            last_node.right = new_node
+            raiz.der = insertar_marvel(raiz.der, no[0], no[1], no[2])
+        return raiz
 
-def search_node(root, question):
-    current_node = root
-    while current_node is not None:
-        if question.lower() == current_node.question.lower():
-            return current_node
-        elif question.lower() < current_node.question.lower():
-            current_node = current_node.left
-        else:
-            current_node = current_node.right
-    return None
+def buscar_marvel(raiz, pregunta):
+    """busca un nodo en el arbol"""
+    if raiz is None or raiz.info == pregunta:
+        return raiz
+    elif pregunta[0] == "si":
+        return buscar_marvel(raiz.izq, pregunta[1])
+    else:
+        return buscar_marvel(raiz.der, pregunta[2])
 
-# Crear el árbol binario
-root = Node("¿El superhéroe es adecuado para misiones de destrucción?")
-add_node(root, "¿Tiene el poder para destruir ejércitos completos?", Node("Thor"))
-add_node(root, "¿Es una excelente opción para misiones de destrucción?", Node("Hulk"))
-add_node(root, "¿Es ideal para misiones intergalácticas en equipo?", Node("Khan"))
-add_node(root, "¿Es excelente en misiones de recuperación donde se requiere no ser detectado?", Node("Ant-Man"))
-add_node(root, "¿Es un supersoldado de ética incorruptible ideal para comandar misiones de defensa y de recuperación?", Node("Capitán América"))
-add_node(root, "¿Es muy poderosa y puede viajar por las distintas galaxias?", Node("Capitana Marvel"))
-add_node(root, "¿Es muy hábil y puede ser útil para varias misiones?", Node("Khan"))
-add_node(root, "¿Es la persona indicada para misiones de recuperación donde requiera infiltrarse con personas del lugar?", Node("The Winter Soldier"))
-add_node(root, "¿Es la opción más lógica para elegir la próxima acción y moverse rápidamente de un lugar a otro?", Node("Nick Fury"))
+arbol = NodoArbol("Preg1")
+arbol.izq = NodoArbol("Preg2")
+arbol.izq.izq = NodoArbol("Hulk")
+arbol.izq.der = NodoArbol("Thor")
+arbol.der = NodoArbol("Preg3")
+arbol.der.izq = NodoArbol("IronMan")
+arbol.der.der = NodoArbol("Capitan")
 
-# Buscar un nodo en el árbol binario
-search_node(root, "¿Es una excelente opción para misiones de destrucción?")
-
-# Salida
-print(search_node(root, "¿Es una excelente opción para misiones de destrucción?").left.question)
+arbol = insertar_marvel(arbol, "¿El superhéroe tiene telarañas?", 
+                        ["Spiderman", None, None], ["Capitan", None, None])
--- pregunta1.py
+++ pregunta1.py
@@ -8,10 +8,24 @@ class NodoArbol(object):
         self.info = info
         self.izq = None
         self.der = None
-        self.altura = 0
+
+def insertar_preguntas(raiz, dato):
+    """inserta un nodo en el arbol"""
+    if raiz is None:
+        nuevo_nodo=NodoArbol(dato)
+        return nuevo_nodo
+    else:
+        nodo_actual = raiz
+        # si la respuesta a la pregunta es si mover a la izquierda
+        while nodo_actual is not None:
+            ultimo_nodo = nodo_actual
+            if dato[0] == "si":
+                nodo_actual = nodo_actual.izq
+            # si la respuesta a la pregunta es no mover a la derecha
+            else:
+                nodo_actual = nodo_actual.der
 
 def insertar_marvel(raiz, dato):
-    nuevo_nodo=NodoArbol(dato)
     """inserta un nodo en el arbol"""
     if raiz is None:
         nuevo_nodo=NodoArbol(dato)
@@ -41,4 +55,7 @@ def buscar_marvel(raiz, dato):
             nodo_actual = nodo_actual.der
     return None
 
+
+# Crear el árbol binario
+raiz = NodoArbol("¿El superhéroe es adecuado para misiones de destrucción?")
 

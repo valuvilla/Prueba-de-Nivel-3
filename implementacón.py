4b2ff8db473969c9037ee368d914b527a08c6181
valuvilla <valuvp@hotmail.com>
2023-04-25 09:57:02 +0200

    cambios

--- implementacón.py
+++ implementacón.py
@@ -3,41 +3,45 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
+    
+    def is_leaf(self):
+        return self.left is None and self.right is None
 
-class Tree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, node, data):
-        if node is None:
-            return Node(data)
-        if data.lower() == 'yes':
-            node.left = self.insert(node.left, input("¿Cuál es la pregunta para el subarbol izquierdo? "))
-        elif data.lower() == 'no':
-            node.right = self.insert(node.right, input("¿Cuál es la pregunta para el subarbol derecho? "))
+def assign_hero(mission):
+    root = Node("¿La misión es intergaláctica?")
+    root.left = Node("¿La misión es de recuperación y no se puede ser detectado?")
+    root.right = Node("¿La misión es de destrucción?")
+    
+    root.left.left = Node("¿Se requiere infiltración con personas del lugar?")
+    root.left.right = Node("¿Es Khan el solicitante?")
+    
+    root.left.right.left = Node("Khan")
+    root.left.right.right = Node("Ant-Man")
+    
+    root.left.left.left = Node("The Winter Soldier")
+    root.left.left.right = Node("Capitana Marvel")
+    
+    root.right.left = Node("¿Se requiere de un líder para planear la misión?")
+    root.right.right = Node("¿Es Nick Fury el solicitante?")
+    
+    root.right.left.left = Node("Iron Man")
+    root.right.left.right = Node("Capitán América")
+    
+    root.right.right.left = Node("Thor")
+    root.right.right.right = Node("The Incredible Hulk")
+    
+    current_node = root
+    while not current_node.is_leaf():
+        answer = input(current_node.data + " (s/n) ")
+        if answer.lower() == 's':
+            current_node = current_node.left
+        elif answer.lower() == 'n':
+            current_node = current_node.right
         else:
-            print("Superhéroe encontrado: " + data)
-            return Node(data)
+            print("Respuesta no válida")
+    
+    return current_node.data
 
-    def search(self, node, data):
-        if node is None:
-            return None
-        if node.data.lower() == data.lower():
-            return node
-        elif data.lower() == 'yes':
-            return self.search(node.left, input(node.data + " (s/n) "))
-        elif data.lower() == 'no':
-            return self.search(node.right, input(node.data + " (s/n) "))
-
-tree = Tree()
-tree.root = Node(input("¿Cuál es la pregunta inicial? "))
-tree.insert(tree.root, input("¿Cuál es la respuesta para el subarbol izquierdo? "))
-tree.insert(tree.root, input("¿Cuál es la respuesta para el subarbol derecho? "))
-
-hero = tree.search(tree.root, input("¿Cuál es tu pregunta? (Escribe 'yes' o 'no') "))
-
-if hero:
-    print("Superhéroe encontrado: " + hero.data)
-else:
-    print("No se encontró ningún superhéroe que coincida con las respuestas dadas.")
-
+mission = input("Introduce la descripción de la misión: ")
+hero = assign_hero(mission)
+print(f"El superhéroe asignado para la misión es: {hero}")

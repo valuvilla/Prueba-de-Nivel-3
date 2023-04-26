652b0c65505971a03b42206db347248688144c01
valuvilla <valuvp@hotmail.com>
2023-04-26 17:01:54 +0200

    c3

--- pregunt3.py
+++ pregunt3.py
@@ -2,7 +2,7 @@ def suma(num1, num2):
     try:
         resultado = float(num1) + float(num2)
         return resultado
-    except TypeError:
+    except ValueError:
         print("Error: Tipo de dato no válido")
         return None
 
@@ -34,31 +34,5 @@ def division(num1, num2):
         print("Error: Tipo de dato no válido")
         return None
 
-
-from ast import main
-import sys
-
-print('\033[1m''\033[96m'"EJERCICIO 1" '\033[96m''\033[0m')
-x=7
-y=0
-
-def error_1(x,y):
-    #Uso de try and except para controlar las excepciones
+print(resta(1,"Hola"))
     
-    try:
-        x/y#se evalua la division
-
-
-    except: #Controlamos que el denominador no sea 0 mediante ZeroDivisionError que permite que el programa lo dectecte
-        raise ZeroDivisionError("Dividir entre 0 no es posible") #cuando se divide entre cero, el programa lanzara este mensaje. Además de que se guardará el error mediante el file=sys.stderr
-        #Al incluir ZeroDivisionError no tenemos porque preocuparnos por añadir el file=sys.stdrr pues estamos utilizando un comando que ya tiene almacena este tipo de error
-
-        sys.exit() #Salimos del try and except
-
-
-#comprobamos la funcion funciona
-print(error_1(x,y))
-
-#importamos a main
-if __name__=="__main__":
-   main()
